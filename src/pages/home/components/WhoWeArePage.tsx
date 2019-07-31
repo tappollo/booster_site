e93d3a8893fe687939df3b19512f47322aff64be
zitao xiong <caoer115@gmail.com>
2019-07-31 10:57:41 +0800

    implement new home page

--- src/pages/home/components/WhoWeArePage.tsx
+++ src/pages/home/components/WhoWeArePage.tsx
@@ -1,6 +1,10 @@
 import Content from "../../../components/Content";
 import * as React from "react";
-import { layoutBreak, shrinkBreak, useMobileLayout } from "../../../functions/styles";
+import {
+  layoutBreak,
+  shrinkBreak,
+  useMobileLayout
+} from "../../../functions/styles";
 import styled, { css } from "styled-components";
 import WhoWeAreAnimation from "./WhoWeAreAnimation";
 import NavigationRow from "./NavigationRow";
@@ -96,30 +100,26 @@ const PlatformImg = styled.img`
 
 const Desc = (props: any) => (
   <Desc.P {...props}>
-    Scaffolding code, prebuilt UI components and integrated Cl/CD (continuous
-    integration/continuous delivery) for rapid development in iOS, Android and
-    web. Booster helps you develop native apps for all platforms in half the
-    time and half the manpower.
+    Integrated scaffolding codes, UI components, integration and delivery
+    pipeline for fast prototyping in iOS, android and mobile web.
   </Desc.P>
 );
 
 Desc.P = styled.p`
-  font-family: "SF Compact Display", sans-serif;
-  font-size: 15px;
-  font-weight: normal;
+  font-family: "Hind Guntur", sans-serif;
+  font-weight: 700;
+  font-size: 20px;
   margin-bottom: 15px;
   @media (min-width: ${shrinkBreak}px) {
     font-size: 20px;
-    font-weight: bold;
   }
   @media (min-width: ${layoutBreak}px) and (max-width: ${shrinkBreak}px) {
     font-size: 18px;
-    font-weight: 500;
   }
 `;
 
 const GetStarted = styled.a`
-  font-family: "SF Compact Display", sans-serif;
+  font-family: "Hind Guntur", sans-serif;
   background-color: #ffb000;
   margin: 15px 0 30px;
   color: white;
@@ -167,11 +167,130 @@ Github.Title = styled.div`
 `;
 
 Github.Container = styled.div`
-  margin-bottom: 40px;
+  margin-bottom: 30px;
   flex-direction: row;
   align-items: center;
-  font-family: "SF Compact Display", sans-serif;
+  justify-content: flex-end;
+  font-family: "Hind Guntur", sans-serif;
+  font-weight: 400;
   font-size: 15px;
+`;
+
+const PowerBy = () => {
+  return (
+    <PowerBy.Container>
+      <PowerBy.Text>Powered by&nbsp;&nbsp;&nbsp;</PowerBy.Text>
+      <PowerBy.React />
+      <PowerBy.Text>&nbsp;&nbsp;&&nbsp;&nbsp;</PowerBy.Text>
+      <PowerBy.Firebase />
+    </PowerBy.Container>
+  );
+};
+
+PowerBy.Container = styled.div`
+  flex-direction: row;
+  margin: 30px 0;
+`;
+
+PowerBy.Text = styled.span`
+  font-family: "Hind Guntur", sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  color: #ffffff;
+`;
+PowerBy.React = styled.img.attrs({
+  src: require("../../../assets/images/react-native-icon@3x.png"),
+  width: 125,
+  height: 21,
+  alt: "React"
+})``;
+
+PowerBy.Firebase = styled.img.attrs({
+  src: require("../../../assets/images/firebase-icon@3x.png"),
+  width: 80,
+  height: 21,
+  alt: "Firebase"
+})``;
+
+const WithWithoutBooster = () => {
+  return (
+    <WithWithoutBooster.Container>
+      <WithWithoutBooster.Row>
+        <WithWithoutBooster.Head>
+          <WithWithoutBooster.Square style={{ backgroundColor: "#660C6E" }} />
+          <WithWithoutBooster.TitleWithout>
+            Without Booster
+          </WithWithoutBooster.TitleWithout>
+        </WithWithoutBooster.Head>
+        <WithWithoutBooster.WithoutBoosterImage />
+      </WithWithoutBooster.Row>
+      <WithWithoutBooster.Row>
+        <WithWithoutBooster.Head>
+          <WithWithoutBooster.Square style={{ backgroundColor: "#FF00C9" }} />
+          <WithWithoutBooster.TitleWith>
+            With Booster
+          </WithWithoutBooster.TitleWith>
+        </WithWithoutBooster.Head>
+        <WithWithoutBooster.WithBoosterImage />
+      </WithWithoutBooster.Row>
+    </WithWithoutBooster.Container>
+  );
+};
+
+WithWithoutBooster.Container = styled.div`
+  flex-direction: column;
+  background-color: rgba(0, 0, 0, 0.3);
+  margin: 0px -20px;
+  padding: 20px;
+  @media (min-width: ${layoutBreak}px) {
+    flex-direction: row;
+    padding: 40px;
+  }
+`;
+
+WithWithoutBooster.Row = styled.div`
+  margin: 20px 40px;
+`;
+
+WithWithoutBooster.Head = styled.div`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+WithWithoutBooster.Square = styled.div`
+  height: 14px;
+  width: 12px;
+  margin-right: 10px;
+`;
+WithWithoutBooster.TitleWith = styled.span`
+  font-family: "HindGuntur", sans-serif;
+  font-weight: 700;
+  font-size: 30px;
+  color: #ff00c9;
+  letter-spacing: 0.41px;
+`;
+WithWithoutBooster.TitleWithout = styled.span`
+  font-family: "HindGuntur", sans-serif;
+  font-weight: 400;
+  font-size: 30px;
+  opacity: 0.62;
+  color: #b620e0;
+  letter-spacing: 0.29px;
+`;
+WithWithoutBooster.WithoutBoosterImage = styled.img.attrs({
+  src: require("../../../assets/images/without-booster-graph@2x.png"),
+  alt: "Without Booster"
+})`
+  max-width: 100%;
+  height: auto;
+`;
+
+WithWithoutBooster.WithBoosterImage = styled.img.attrs({
+  src: require("../../../assets/images/with-booster-graph@2x.png"),
+  alt: "Without Booster"
+})`
+  max-width: 100%;
+  height: auto;
 `;
 
 const WhoWeArePage = () => {
@@ -180,7 +299,7 @@ const WhoWeArePage = () => {
     <Content>
       <NavigationRow />
       <Container
-        style={{ minHeight: mobileLayout ? "none" : window.innerHeight - 80 }}
+        style={{ minHeight: mobileLayout ? "none" : window.innerHeight - 40 }}
       >
         <Left>
           {!mobileLayout && <Header />}
@@ -188,14 +307,16 @@ const WhoWeArePage = () => {
           <GetStarted href="https://tappollo.github.io/booster" target="_blank">
             Documentation
           </GetStarted>
-          <Github />
+          <PowerBy />
         </Left>
         <div style={{ height: 30, width: 30 }} />
         <Right>
           {mobileLayout && <Header />}
+          <Github />
           <WhoWeAreAnimation />
         </Right>
       </Container>
+      <WithWithoutBooster />
     </Content>
   );
 };
